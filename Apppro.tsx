f617005965d6c305742f02b3b467c0a27921a49e
zestymec <m.umer394aziz@gmail.com>
2026-04-08 11:36:40 +0500

    page ui updated

--- Apppro.tsx
+++ Apppro.tsx
@@ -3,19 +3,31 @@ import React from "react";
 import {
   Text,
   View,
-  SafeAreaView
+  SafeAreaView,
+  StyleSheet
 } from 'react-native'
 
 function Apppro() {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={Style.container}> 
       <View>
-  
+        <Text style={Style.textStyle}>umer</Text> 
       </View>
     </SafeAreaView>
   )
 }
 
+const Style = StyleSheet.create({ 
+  container: {                    
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  textStyle: {
+    color: 'black',
+    fontSize: 20,
+  
 
+  }                               
+});                               
 
 export default Apppro;
